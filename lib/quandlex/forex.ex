f9b2166d6838418dc6e696cd288921dd9da89f7e
ontofractal <ontofractal@protonmail.com>
2018-05-25 13:22:39 +0100

    add currency pair mappings for ECB dataset

--- lib/quandlex/forex.ex
+++ lib/quandlex/forex.ex
@@ -3,10 +3,12 @@ defmodule Quandlex.Forex do
   Utility module to query historical data for foreign exchange rates using Quandl free APIs
   """
 
+
   def get_all_daily(from, to, opts = [source: source]) do
     symbol = generate_symbol(from, to, opts)
     Quandlex.Client.timeseries(source, symbol)
   end
+
 
   def generate_symbol(from, to, source: "BOE") do
     case {from, to} do
@@ -46,6 +48,44 @@ defmodule Quandlex.Forex do
       {"CHF", "USD"} -> "DEXSZUS"
       {"THB", "USD"} -> "DEXTHUS"
       {"VEF", "USD"} -> "DEXVZUS"
+    end
+  end
+
+  def generate_symbol(from, to, source: "ECB") do
+    case {from, to} do
+      {"AUD", "EUR"} -> "EURAUD"
+      {"BGN", "EUR"} -> "EURBGN"
+      {"BRL", "EUR"} -> "EURBRL"
+      {"CAD", "EUR"} -> "EURCAD"
+      {"CHF", "EUR"} -> "EURCHF"
+      {"CNY", "EUR"} -> "EURCNY"
+      {"CZK", "EUR"} -> "EURCZK"
+      {"DKK", "EUR"} -> "EURDKK"
+      {"GBP", "EUR"} -> "EURGBP"
+      {"HKD", "EUR"} -> "EURHKD"
+      {"HRK", "EUR"} -> "EURHRK"
+      {"HUF", "EUR"} -> "EURHUF"
+      {"IDR", "EUR"} -> "EURIDR"
+      {"ILS", "EUR"} -> "EURILS"
+      {"INR", "EUR"} -> "EURINR"
+      {"ISK", "EUR"} -> "EURISK"
+      {"JPY", "EUR"} -> "EURJPY"
+      {"KRW", "EUR"} -> "EURKRW"
+      {"LTL", "EUR"} -> "EURLTL"
+      {"MXN", "EUR"} -> "EURMXN"
+      {"MYR", "EUR"} -> "EURMYR"
+      {"NOK", "EUR"} -> "EURNOK"
+      {"NZD", "EUR"} -> "EURNZD"
+      {"PHP", "EUR"} -> "EURPHP"
+      {"PLN", "EUR"} -> "EURPLN"
+      {"RON", "EUR"} -> "EURRON"
+      {"RUB", "EUR"} -> "EURRUB"
+      {"SEK", "EUR"} -> "EURSEK"
+      {"SGD", "EUR"} -> "EURSGD"
+      {"THB", "EUR"} -> "EURTHB"
+      {"TRY", "EUR"} -> "EURTRY"
+      {"USD", "EUR"} -> "EURUSD"
+      {"ZAR", "EUR"} -> "EURZAR"
     end
   end
 end
