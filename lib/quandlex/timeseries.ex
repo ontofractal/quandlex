379102826c5d4cb1a2dd1cd5a83e6d1ee3a35934
ontofractal <ontofractal@protonmail.com>
2018-05-29 15:45:44 +0100

    improve developer experience: unwrap nested fields for corresponding functions

--- lib/quandlex/timeseries.ex
+++ lib/quandlex/timeseries.ex
@@ -5,20 +5,21 @@ defmodule Quandlex.Timeseries do
   plug Tesla.Middleware.JSON
 
   def get_data(database_code, dataset_code) do
-    call("/datasets/#{database_code}/#{dataset_code}/data.json")
+    call("/datasets/#{database_code}/#{dataset_code}/data.json", "dataset_data")
   end
 
   def get_database_metadata(database_code) do
-    call("/databases/#{database_code}.json")
+    call("/databases/#{database_code}.json", "database")
   end
 
   def get_dataset_metadata(database_code, dataset_code) do
-    call("/datasets/#{database_code}/#{dataset_code}/metadata.json")
+    call("/datasets/#{database_code}/#{dataset_code}/metadata.json", "dataset")
   end
 
-  def call(url) do
-    with {:ok, tesla_env} <- get(url) do
-      {:ok, tesla_env.body}
+  def call(url, field_to_unwrap) do
+    with {:ok, tesla_env} <- get(url),
+         response <- tesla_env.body[field_to_unwrap] do
+      {:ok, response}
     else
       err -> err
     end
