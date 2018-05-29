18ee1a379720a14d755567b6cba50d556c5789a2
ontofractal <ontofractal@protonmail.com>
2018-05-29 15:06:21 +0100

    add tests for timeseries metadata and database metadata options

--- test/quandlex_test.exs
+++ test/quandlex_test.exs
@@ -4,6 +4,16 @@ defmodule QuandlexTest do
 
   test "get timeseries" do
     assert %{"dataset_data" => %{"data" => data, "start_date" => _}} =
-             Quandlex.Client.timeseries("BOE", "XUDLJYS", "json")
+             Quandlex.Client.timeseries("BOE", "XUDLJYS")
+  end
+
+  test "get timeseries with metadata" do
+    assert %{"dataset_data" => %{"data" => data, "start_date" => _}} =
+             Quandlex.Client.timeseries("BOE", "XUDLJYS", metadata: true)
+  end
+
+  test "get database metadata" do
+    assert %{"dataset_data" => %{"data" => data, "start_date" => _}} =
+             Quandlex.Client.timeseries("BOE", "XUDLJYS", metadata: :database)
   end
 end
